3fbd8532d1360e190474c0034368db0e4082e40f
Preston <prescarlton@gmail.com>
2022-08-17 21:41:00 -0400

    adds currency, TMP styling

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
-import { FaPlusCircle } from "react-icons/fa"
+import { FaMoneyBill, FaPlusCircle } from "react-icons/fa"
+import { GiGoldBar, GiGoldNuggets, GiMoneyStack } from "react-icons/gi"
 import Button from "../components/common/Button"
 import PageTitle from "../components/common/PageTitle"
 import InventoryItem from "../components/inventory/InventoryItem"
@@ -26,20 +27,62 @@ const InventoryPage = () => {
   return (
     <Page>
       <PageTitle title="Inventory" />
-      <div className="mt-4 mb-8 flex items-center">
-        <Button variant="outlined" onClick={handleOpenAddModal}>
-          <FaPlusCircle />
-          Add Item
-        </Button>
+
+      <div className="flex flex-col items-start gap-2 my-4">
+        <div className="flex items-center gap-1">
+          <h1 className="text-xl">Currency</h1>
+          <Button variant="text">
+            <GiMoneyStack />
+            Add Currency
+          </Button>
+        </div>
+        <div
+          className="flex items-center gap-2
+     w-full md:w-auto"
+        >
+          <div
+            className="bg-light-bg-alt dark:bg-dark-bg-alt px-4 py-2
+    shadow-hard rounded-lg text-[#E5E4E2]"
+          >
+            3 P
+          </div>
+          <div
+            className="bg-light-bg-alt dark:bg-dark-bg-alt px-4 py-2
+    shadow-hard rounded-lg text-[gold]"
+          >
+            94 G
+          </div>
+          <div
+            className="bg-light-bg-alt dark:bg-dark-bg-alt px-4 py-2
+    shadow-hard rounded-lg text-[silver]"
+          >
+            4 S
+          </div>
+          <div
+            className="bg-light-bg-alt dark:bg-dark-bg-alt px-4 py-2
+    shadow-hard rounded-lg text-[#B87333]"
+          >
+            13 C
+          </div>
+        </div>
       </div>
-      <div className="flex gap-4 flex-wrap">
-        {fakeItems.map((item) => (
-          <InventoryItem
-            key={item.id}
-            item={item}
-            onClick={handleOpenItemModal}
-          />
-        ))}
+      <div className="flex flex-col items-start gap-2">
+        <div className="flex items-center gap-1">
+          <h1 className="text-xl">Items</h1>
+          <Button variant="text" onClick={handleOpenAddModal}>
+            <FaPlusCircle />
+            Add Item
+          </Button>
+        </div>
+        <div className="flex gap-4 flex-wrap">
+          {fakeItems.map((item) => (
+            <InventoryItem
+              key={item.id}
+              item={item}
+              onClick={handleOpenItemModal}
+            />
+          ))}
+        </div>
       </div>
       <ItemModal
         isOpen={isItemModalOpen}
